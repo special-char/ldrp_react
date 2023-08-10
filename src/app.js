d1c300358470eb24c8eb562a1bd699171c5d5721
Yagnesh Modh <contact@thespecialcharacter.com>
2023-08-10 11:23:29 +0530

    setup webpack

---
+++ src/app.js
@@ -0,0 +1,7 @@
+export const a = 1;
+
+export const b = 2;
+
+// per page only 1 export default
+
+// export default { a, b}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import {a, b} from './app'
+
+console.log(a);
+console.log(b);
